1355e3aec2f2432fa651b79abccfc3e2300c99e1
Cassie Dunning <hello.cassdunning@gmail.com>
2022-12-12 09:58:26 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,27 @@
+//#2: Mouthy Functions
+/* 
+
+It will work very similarly to the findHalf() function shown above. What you need to do is complete the following tasks:
+
+When you run the code, you should see the answer in the console.
+*/
+
+// Define a parameter that will hold the value of the original number.
+const quarterValue = (value) => {
+    //Divide that number by 4
+    const quarter = value / 4
+
+    //Return the quartered number
+    return quarter
+
+}
+//Store the returned number in a variable
+const returnedValue = quarterValue(56)
+// Invoke the function and store the return value
+
+
+//Plug that variable into the parenthesis for the console.log()
+console.log(returnedValue)
+
+
+
